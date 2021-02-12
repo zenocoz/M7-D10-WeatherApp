280bf1eccb9dbc0aa2b559625157118c5e01a2dc
zenocoz <federico.soncini@gmail.com>
2021-02-12 11:17:47 +0100

    getting latitude and longitude

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import { render } from "react-dom"
+import { useState, useEffect } from "react"
+import MyNav from "./components/Navbar/Navbar"
 
 function App() {
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLatitude(position.coords.latitude)
+      setLongitude(position.coords.longitude)
+      console.log("Latitude is :", latitude)
+      console.log("Longitude is :", longitude)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyNav />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
